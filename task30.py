9eee38f972582f38c08b90b6e9dc019d3360dbe1
zhannas <zhannas@gmail.com>
2023-06-08 17:10:22 +0200

    task30

---
+++ task30.py
@@ -0,0 +1,12 @@
+# Задача 30:  Заполните массив элементами арифметической прогрессии. Её первый элемент, 
+#разность и количество элементов нужно ввести с клавиатуры. Формула для получения n-го члена 
+#прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+first_el = int(input('Enter the first element: '))
+d = int(input('Enter the difference: '))
+a = int(input('Enter the amount of elements: '))
+list = []
+# for i in range(a):
+#     list.append(firs_el + i*d)
+# print(list)
